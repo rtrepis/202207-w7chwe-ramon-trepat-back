fd93b4c3d00a3bdbe0ce086d41787a595ec7ba2b
Ramon Trepat <r.trepis@gmail.com>
2022-08-20 14:40:57 +0200

    Added users Router

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,12 +2,15 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { generalError, notFoundError } from "./middlewares/errors";
+import usersRouter from "./routers/usersRouter";
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/users", usersRouter);
 
 app.use(notFoundError);
 app.use(generalError);
